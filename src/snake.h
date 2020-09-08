5119aab133001e5a5ae2ba0163c53ea6de648931
Joker2770 <1214220480@qq.com>
2020-09-08 09:31:49 +0800

    test: add snake define. :sparkles:

--- src/snake.h
+++ src/snake.h
@@ -1,10 +1,10 @@
 /*************************************************************************
-    > File Name: rungame.cpp
+    > File Name: snake.h
     > Author: Jintao Yang
     > Mail: 18608842770@163.com 
-    > Created Time: Mon Sep  7 08:40:32 2020
+    > Created Time: Tue Sep  8 09:03:57 2020
  ************************************************************************/
-
+ 
 /*
 MIT License
 
@@ -29,61 +29,21 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#ifdef _WIN32
-//Windows
-extern "C"
+#include<iostream>
+using namespace std;
+
+typedef struct snake_node
 {
-#include "SDL.h"
+	int x_pos;
+	int y_pos;
+	struct snake_node* next;
+}SnakeNode, *SnakeList;
+
+class Snake
+{
+	public:
+
+	private:
+		SnakeList m_snake;
+
 };
-#else
-//Linux
-//Linux C++
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-	
-#include<SDL2/SDL.h>
-
-//Linux C++
-#ifdef __cplusplus
-};
-#endif
-#endif
-
-#include <stdio.h>
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-int main(int argc, char *argv[])
-{
-	SDL_Window *window = nullptr;
-	SDL_Surface *screenSurface = nullptr;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	else
-	{
-		window = SDL_CreateWindow("SDL2snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (nullptr == window)
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 255, 255));
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-			//Delay two seconds
-			SDL_Delay(2000);
-		}
-	}
-
-	SDL_FreeSurface(screenSurface);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
-}
