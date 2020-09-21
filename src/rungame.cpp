70d12a367fad44209cd35e0c8a99309a4f77715d
Joker2770 <1214220480@qq.com>
2020-09-21 08:27:32 +0800

    docs: rename window title. :memo:

--- src/rungame.cpp
+++ src/rungame.cpp
@@ -244,7 +244,7 @@ bool init()
 		}
 
 		//Create window
-		gWindow = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("SDL2snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
